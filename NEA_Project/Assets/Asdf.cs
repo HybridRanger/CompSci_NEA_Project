32a62ea3d3880ed31b31e8b0b020a724f1fac6d5
HybridRanger <hybridranger64@gmail.com>
2018-11-08 14:56:17 +0000

    2D orbits

---
+++ NEA_Project/Assets/Asdf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asdf : MonoBehaviour {
+
+    public Vector2 startForce;
+    public Rigidbody2D rb;
+
+	void Start () {
+        rb.AddForce(startForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NEA_Project/Assets/Attractor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+    
+    public Rigidbody2D rb;
+
+    private float G = 6.67e-11f;
+
+	void FixedUpdate () {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach (Attractor attractor in attractors)
+        {
+            if (attractor != this)
+            {
+                Attract(attractor);
+            }
+        }
+	}
+
+    void Attract(Attractor targetObj)
+    {
+        Rigidbody2D rbTarget = targetObj.rb;
+        Vector2 direction = rbTarget.position - rb.position;
+        float distance = direction.magnitude;
+        float magnitude = (rbTarget.mass * rb.mass)/Mathf.Pow(distance, 2);
+        Vector2 force = direction.normalized * magnitude;
+
+        rb.AddForce(force);
+    }
+}
